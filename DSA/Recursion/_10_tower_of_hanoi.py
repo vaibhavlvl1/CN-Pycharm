d328ea4bf7d6f6696c8582718268119bab8e7cdd
vaibhavlvl1 <110812464+vaibhavlvl1@users.noreply.github.com>
2023-01-29 15:59:26 +0530

    .

---
+++ DSA/Recursion/_10_tower_of_hanoi.py
@@ -0,0 +1,11 @@
+
+def tower_of_hanoi(n,a,b,c):
+    if n==1:
+        print(f"move {n}th disk from {a} to {c}")
+        return
+    tower_of_hanoi(n-1, a, c, b)
+    print(f"move {n}th disk from {a} to {c}")
+    tower_of_hanoi(n-1,b,a,c)
+
+
+tower_of_hanoi(3,'a','b','c')
