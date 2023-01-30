07c440b9abce3dffa5497d04c363084c7bdf873f
vaibhavlvl1 <110812464+vaibhavlvl1@users.noreply.github.com>
2023-01-30 14:34:06 +0530

    .

--- DSA/Recursion/_10_tower_of_hanoi.py
+++ DSA/Recursion/_10_tower_of_hanoi.py
@@ -4,8 +4,8 @@ def tower_of_hanoi(n,a,b,c):
         print(f"move {n}th disk from {a} to {c}")
         return
     tower_of_hanoi(n-1, a, c, b)
-    print(f"move {n}th disk from {a} to {c}")
+    print(f"move {n}th disk from {a} to {c}")  # for last disk
     tower_of_hanoi(n-1,b,a,c)
 
 
-tower_of_hanoi(3,'a','b','c')
+tower_of_hanoi(2,'a','b','c')
---
+++ DSA/Recursion/_11_geometric_sum.py
@@ -0,0 +1,17 @@
+"""
+Given k, find the geometric sum i.e.
+
+1 + 1/2 + 1/4 + 1/8 + ... + 1/(2^k)
+
+answer shouldf be in 5 digits after decimal
+"""
+
+
+def geometric_sum(k):
+    if k==0:
+        return 1
+
+    return 1/2**(k)+ geometric_sum(k-1)
+
+
+print("%.5f" % geometric_sum(3))
