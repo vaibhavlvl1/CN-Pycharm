d8d4a397ca6dd64ab00b16a8bd915dcd496c4138
vaibhavlvl1 <acevaibhav14@gmail.com>
2023-04-16 16:09:10 +0530

    laptop

--- Basics/Search_sort/Insertion_sort.py
+++ Basics/Search_sort/Insertion_sort.py
@@ -23,3 +23,18 @@ for t in range(t):
         print(ele, end=" ")
 
     print()
+
+
+"""
+def ins_sort(arr):
+    n = len(arr)
+    for i in range(1,n):
+        key = arr[i]
+        
+        j = i-1
+            
+        while  j>=0 and key < arr[j]:
+            arr[j+1] = arr[j]
+            j-=1
+        arr[j+1] = key
+"""
